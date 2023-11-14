98da1f3672d4dcb374539d2b5960c31728e250fd
akpbadnawar <70686648+akpbadnawar@users.noreply.github.com>
2023-11-14 12:36:46 +0530

    changes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -40,7 +40,7 @@ export default function ProfilePage() {
 
             <button onClick={getUserDetails}
             className="text-white bg-purple-900 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-4 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            >GetUserDetails</button>
+            >Get User Details</button>
         </div>
     )
 }
