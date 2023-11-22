21a7a00da62e95048354ee63e4f758988b0b0333
akpbadnawar <70686648+akpbadnawar@users.noreply.github.com>
2023-11-22 09:31:11 +0530

    updated the verify emails & reset password backend

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,9 +2,7 @@ import {connect} from "@/dbConfig/dbConfig";
 import User from '@/models/userModel';
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from 'bcryptjs';
-
-
-
+import { sendEmail } from "@/helpers/mailer";
 
 
 export async function POST (request: NextRequest){
@@ -35,6 +33,11 @@ export async function POST (request: NextRequest){
       const savedUser = await newUser.save()
 
       console.log(savedUser)
+
+
+      //send verification email
+
+      await sendEmail({email,emailType:'VERIFY',userId: savedUser._id})
 
       return NextResponse.json({
         message:'User created successfull',
